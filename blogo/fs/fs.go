3e17b2339d048819a4ba9a6111ca2fe3a5b71a64
Gustavo L de Mello (Guz) <contact@guz.one>
2025-01-22 18:48:53 -0300

    refactor(blogo,fs): move fs code to it's own package

--- blogo/fs/fs.go
+++ blogo/fs/fs.go
@@ -13,11 +13,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package blogo
+package fs
 
 import (
 	"io/fs"
 )
+
+var ErrNotExist = fs.ErrNotExist
 
 type FS interface {
 	Metadata() Metadata
