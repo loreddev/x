9fbe24cc5ca89758d3693d04dcc79eb291f406d3
Gustavo "Guz" L de Mello <contact@guz.one>
2025-02-24 08:03:45 -0300

    feat(smalltrip): development middleware

---
+++ smalltrip/middleware/dev.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+func Dev(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
+
+var _ Middleware = Dev
